b0735a09565b0cdd1b9228ebe3ac094c6cbc2380
seongjoo.lee@gmail.com <seongjoo.lee@gmail.com@fa407a1a-0fcc-11df-b47a-af0d17c1a499>
2010-02-02 07:48:04 +0000

    Draft

---
+++ src/eyedetect/eyedetect.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import Image
+import numpy as np
+import pywt
+from scipy import ndimage
+import time
+#import pylab
+import scipy.cluster.vq as clusters
+from numpy.linalg import *
+
+
+
---
+++ src/imagefilters/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import Image
+import numpy as np
+#import pywt
+from scipy import ndimage
+import time
+#import pylab
+#import scipy.cluster.vq as clusters
+from numpy.linalg import *
+
+import utils
+
+__all__ = ["sobels", "binaryedge"]
+
