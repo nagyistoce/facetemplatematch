c113d31c2d2183248aabd200e528539f78971bcb
seongjoo.lee@gmail.com <seongjoo.lee@gmail.com@fa407a1a-0fcc-11df-b47a-af0d17c1a499>
2010-02-08 10:36:08 +0000

    FaceFeature module work in progress.

--- src/FeatureExtract/facefeature.py
+++ src/FeatureExtract/facefeature.py
@@ -3,7 +3,7 @@ Created on 2010. 2. 8.
 
 @author: Seongjoo
 '''
-from math import pi, pow, cos, sin
+from math import pi, cos, sin
 import numpy as np
 
 class FaceFeature(object):
@@ -31,26 +31,43 @@ class FaceFeature(object):
         at an image location (x,y)
         '''
         # Discrete rotation angles
-        theta = []
         n = 3 # number of orientation
-        for k in xrange(0, n-1):
-            theta.append(k*pi/n)
+        theta = np.array(np.zeros(n))        
+        for k in xrange(0, n - 1):
+            theta[k] = k * pi / n
         
         # Discrete frequencies
         a = 2 # for octave spacing]
-        f = [1]
-        m=10
-        for k in xrange(1, m-1):
-            f.append(pow(a,-k)*f[0])
+        freq = [1]
+        m = 10
+        for k in xrange(0, m - 1):
+            freq.append(a ** -k * freq[0])
         
         # get x,y coordinate vector from image array
-        image_xy = np.transpose(np.nonzero(self.imageArray))
+        image_xy = np.array(np.transpose(np.nonzero(self.imageArray)))
         
         # compute the new x,y coordinate
-        image_xy_new = image_xy[:]
-        image_xy_new[0,:] = image_xy[0,:]*cos(theta) + image_xy[1,:]*sin(theta)
-            
+        image_xy_new = np.zeros(image_xy.shape)
+                       
+        for angle in theta:
+            image_xy_new[:, 0] = image_xy[:, 1] * cos(angle) + \
+                                image_xy[:, 0] * sin(angle)
+            image_xy_new[:, 1] = -1 * image_xy[:, 0] * sin(angle) + \
+                                image_xy[:, 1] * cos(angle)
         
+        featureParams = []
+        for i in xrange(len(image_xy)):
+            x, y = image_xy[i, 0], image_xy[i, 1]            
+            for f in freq:
+                for angle in theta:                
+                    featureParams.append([x, y, f, angle])
+            self.computeResponse(featureParams[i])                    
+        
+    def computeResponse(self, params):
+        '''
+        compute the response given x,y,f,angle
+        '''
+    
     def computeNormFeatureMatrix(self):
         '''
         Compute normalized feature matrix G'
